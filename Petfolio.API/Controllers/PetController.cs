d6549a0fd61dbd26dfd93c04f8078d6c093422b2
joohxz <joohxz@gmail.com>
2024-09-10 23:50:35 -0300

    Create an endpoint to update a created Pet by his id

--- Petfolio.API/Controllers/PetController.cs
+++ Petfolio.API/Controllers/PetController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Petfolio.Application.UseCases.Pet.Register;
+using Petfolio.Application.UseCases.Pet.Update;
 using Petfolio.Communication.Requests;
 using Petfolio.Communication.Responses;
 
@@ -15,7 +16,16 @@ namespace Petfolio.API.Controllers
         public IActionResult RegisterPet([FromBody] RequestRegisterPetJson request)
         {
             var response = new RegisterPetUseCase().Execute(request);
-            return Ok(response);
+            return Created(string.Empty, response);
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public IActionResult UpdatePet([FromRoute] int id, [FromBody] RequestRegisterPetJson request)
+        {
+            var useCase = new UpdatePetUseCase();
+            useCase.Execute(id, request);
+            return NoContent();
         }
     }
 }
