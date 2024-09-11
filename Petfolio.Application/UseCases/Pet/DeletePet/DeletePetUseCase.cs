3ae0b9cc71a62beeb49db669c83feec06d69fa74
joohxz <joohxz@gmail.com>
2024-09-11 01:09:12 -0300

    Create a Delete Use Case

---
+++ Petfolio.Application/UseCases/Pet/DeletePet/DeletePetUseCase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petfolio.Application.UseCases.Pet.DeletePet
+{
+    public class DeletePetUseCase
+    {
+        public void Execute(int id)
+        {
+            //...
+        }
+    }
+}
