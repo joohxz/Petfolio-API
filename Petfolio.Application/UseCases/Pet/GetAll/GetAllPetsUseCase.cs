84359d1e40a77b21d6968c6554e90934471751cc
joohxz <joohxz@gmail.com>
2024-09-11 01:09:40 -0300

    Create a UseCase to get all pets registereds

---
+++ Petfolio.Application/UseCases/Pet/GetAll/GetAllPetsUseCase.cs
@@ -0,0 +1,28 @@
+﻿using Petfolio.Communication.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petfolio.Application.UseCases.Pet.GetAll
+{
+    public class GetAllPetsUseCase
+    {
+        public ResponseAllPetJson Execute()
+        {
+            return new ResponseAllPetJson
+            {
+                Pets = new List<ResponseShortPetJson>
+                {
+                    new ResponseShortPetJson
+                    {
+                        Id = 1,
+                        Name = "Chiquinho",
+                        Type = Communication.Enums.PetType.Cat,
+                    }
+                }
+            };
+        }
+    }
+}
